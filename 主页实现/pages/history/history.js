5cbdf415a0bbd6ac3bea602db24a52d9ffd4c340
hanji <57144930+ytmf405060@users.noreply.github.com>
2021-11-18 22:04:32 +0800

    2021/11/18 为我的收藏和历史记录加了更多内容

--- 主页实现/pages/history/history.js
+++ 主页实现/pages/history/history.js
@@ -1,6 +1,6 @@
 Page({
     data: {
-      goods:[],
+      num: 5,
       // 输入框的值
       inpValue:""
     },
@@ -15,6 +15,7 @@ Page({
     },
     // 点击 垃圾筒按钮
     use_dustbin(){
+      const that=this
       wx.showModal({
         title: '清除历史记录',
         content: '确定要清除所有历史记录？',
@@ -28,11 +29,26 @@ Page({
               //点击取消,默认隐藏弹框
            } else {
               //点击确定
-              
+              that.setData({num:0})
+              wx.showToast({
+                      title:"清除所有历史",
+                      icon: 'success',
+                      duration: 1500,
+                    })
            }
         },
         fail: function (res) { },//接口调用失败的回调函数
         complete: function (res) { },//接口调用结束的回调函数（调用成功、失败都会执行）
      })
-    }
+    },
+    clickList:function(e){
+      this.setData({
+        num:this.data.num-1
+      })
+          wx.showToast({
+              title:"清除此历史记录",
+              icon: 'success',
+              duration: 1000,
+            })
+   },
   })
