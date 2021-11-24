2d9fc625559ef9f8a60e267eb2de16a3448b9a48
pace03 <xzk_k_k@163.com>
2021-11-24 14:39:55 +0800

    11/24 个人任务代码完成100%

--- addHis.java
+++ addHis.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 public class addHis {
-    @WebServlet(name = "search", urlPatterns = "/search")
+    @WebServlet(name = "addHis", urlPatterns = "/addHis")
     public class login extends HttpServlet {
 
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- searchStar.java
+++ searchStar.java
@@ -8,8 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-public class searchHis {
-    @WebServlet(name = "searchHis", urlPatterns = "/searchHis")
+public class searchStar {
+    @WebServlet(name = "searchStar", urlPatterns = "/searchStar")
     public class login extends HttpServlet {
 
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -24,14 +24,14 @@ public class searchHis {
             Writer out = response.getWriter();
             //获取微信小程序传递的参数值并打印
             String userId = request.getParameter("userId");//用户id
-            ArrayList<HistoryStatistic>userHis=SQL_Operation.searchHis(userId);
-            for(int i=0;i<userHis.size();i++){//后端目前只返回历史记录插件名
-                if(i==userHis.size()-1){
-                    out.write(userHis.get(i).name+"||");
+            ArrayList<StarStatistic>userStar=SQL_Operation.searchStar(userId);
+            for(int i=0;i<userStar.size();i++){//后端目前只返回该用户收藏的插件名
+                if(i==userStar.size()-1){
+                    out.write(userStar.get(i).name+"||");
                     out.flush();
                 }
                 else{
-                    out.write(userHis.get(i).name+"|");
+                    out.write(userStar.get(i).name+"|");
                     out.flush();
                 }
             }
